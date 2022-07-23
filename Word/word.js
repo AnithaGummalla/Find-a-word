8342c7011c954a0ce79c8b2a7d0bc5c6f475805b
AnithaGummalla <109032010+AnithaGummalla@users.noreply.github.com>
2022-07-23 20:14:26 +0530

    Add files via upload

---
+++ Word/word.js
@@ -0,0 +1,38 @@
+const wordList = [
+    {
+        word: 'javascript',
+        hint: 'programming language'
+    },
+    {
+        word: 'guitar',
+        hint: 'a musical instrument'
+    },
+    {
+        word: 'venus',
+        hint: 'planet of our solar system'
+    },
+    {
+        word: 'gold',
+        hint: 'a yellow precious metal'
+    },
+    {
+        word: 'eyetex',
+        hint: 'costume'
+    },
+    {
+        word: 'search',
+        hint: 'act to find something'
+    },
+    {
+        word: 'server',
+        hint: 'realted to computer or system'
+    },
+    {
+        word: 'svg',
+        hint: 'a vector image format'
+    },
+    {
+        word: 'jpeg',
+        hint: 'a small image file format'
+    }
+]
